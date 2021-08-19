1dccc99953b6845e586d792040e356d7497b99ca
kamarudin <kamarudin@amikom.ac.id>
2021-08-19 15:53:02 +0700

    Penambahan dukungan multi account

--- src/signalr.util.js
+++ src/signalr.util.js
@@ -1,1 +1,1 @@
-const signalr=require("node-signalr"),serverHub="ServerHub",signalRClient=new signalr.client("http://127.0.0.1:8282/signalr",[serverHub]);module.exports={signalRClient:signalRClient,serverHub:serverHub};
+require("dotenv").config();const{PORT:PORT}=process.env,signalr=require("node-signalr"),serverHub="ServerHub",signalRClient=new signalr.client(`http://127.0.0.1:${PORT}/signalr`,[serverHub]);module.exports={signalRClient:signalRClient,serverHub:serverHub};
